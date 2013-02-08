cd952370e7b1d099f3e2914032c57eb0bef74970
Israel Teixeira <israelst@gmail.com>
2013-02-07 23:10:48 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChadomeubebeComBr::Application.config.secret_token = '4ac65731f6fa50affe16c39c6b6333fbee0245c59f229ba3a7bb0cd4afe70492611e08b7106772dd8c7ae3c0e8cb3da8681d2ea1b6247cfaf39c19e1d6046e5d'

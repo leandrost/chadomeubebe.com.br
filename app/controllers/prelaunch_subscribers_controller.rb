38fbf4a69f6f4255aadbb721cafd8792415bd21a
leandrost <leandro.s.thimoteo@gmail.com>
2013-02-24 23:03:10 -0300

    add prelaunch subscriber controller

--- app/controllers/prelaunch_subscribers_controller.rb
+++ app/controllers/prelaunch_subscribers_controller.rb
@@ -1,2 +1,16 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
 class PrelaunchSubscribersController < ApplicationController
+  def new
+    @subscriber = PrelaunchSubscriber.new
+  end
+
+  def create
+    @subscriber = PrelaunchSubscriber.new(params[:prelaunch_subscriber])
+    if @subscriber.save
+      flash[:success] = I18n.t('subscriber_success_message')
+    end
+    redirect_to root_path
+  end
 end

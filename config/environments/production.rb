1cd369313e2d6ee36ef9869abc5ffecf92c38369
Israel Teixeira <israelst@gmail.com>
2013-03-26 11:29:48 -0300

    Fallback to assets pipeline if precompiled asset is missed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ ChadomeubebeComBr::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # TODO turn it to false again and solve heroko deploy
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
